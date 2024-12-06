5b89981cacfd5dabdbee45515df2f84d10e63c14
Eloise Ferreira <eloiseferreira045@gmail.com>
2024-12-06 11:50:18 -0300

    first commit: set database and packages

---
+++ src/main/java/com/eloisefeh/parking_control/models/ParkingSpotModel.java
@@ -0,0 +1,9 @@
+package com.eloisefeh.parking_control.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel {
+}
