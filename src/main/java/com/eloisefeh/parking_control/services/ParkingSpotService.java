498ca5b25c3aeb9d5a9720b45111d13a2d87a795
Eloise Ferreira <eloiseferreira045@gmail.com>
2024-12-11 12:56:51 -0300

    create parking spot service

---
+++ src/main/java/com/eloisefeh/parking_control/services/ParkingSpotService.java
@@ -0,0 +1,14 @@
+package com.eloisefeh.parking_control.services;
+
+import com.eloisefeh.parking_control.repositories.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+}
