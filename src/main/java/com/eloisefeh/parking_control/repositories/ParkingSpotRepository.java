1a745dd33c042be066d90c09463d5fd82293a381
Eloise Ferreira <eloiseferreira045@gmail.com>
2024-12-11 12:56:15 -0300

    create parking post repository

---
+++ src/main/java/com/eloisefeh/parking_control/repositories/ParkingSpotRepository.java
@@ -0,0 +1,12 @@
+package com.eloisefeh.parking_control.repositories;
+
+import com.eloisefeh.parking_control.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+}
