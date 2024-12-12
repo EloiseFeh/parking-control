318f4577ddb842a781f8da576771a2885d423d6d
Eloise Ferreira <eloiseferreira045@gmail.com>
2024-12-12 11:59:11 -0300

    GET: list all parking spots

--- src/main/java/com/eloisefeh/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/eloisefeh/parking_control/services/ParkingSpotService.java
@@ -2,8 +2,12 @@ package com.eloisefeh.parking_control.services;
 
 import com.eloisefeh.parking_control.models.ParkingSpotModel;
 import com.eloisefeh.parking_control.repositories.ParkingSpotRepository;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ParkingSpotService {
@@ -18,5 +22,21 @@ public class ParkingSpotService {
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
         return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(@NotBlank String apartment, @NotBlank String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
     }
 }
